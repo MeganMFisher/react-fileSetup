36b773745eafb7e2004ddd9ce22818d113720b9a
MeganMFisher <meganmichelle318@gmail.com>
2017-08-17 07:12:04 -0600

    proxy working!

--- app/server/server.js
+++ app/server/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
 // app.use( express.static( __dirname + '/../public/' ) )
+app.use(cors());
 
 //cors
 
